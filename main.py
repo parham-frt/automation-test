b8945d6966109c2160d358efb980149559153398
p.forootanzadeh <parhamforootan@gmail.com>
2023-11-14 12:05:18 +0330

    add

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from selenium webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from chromedriver_py import binary_path
+from selenium.webdriver.common.keys import Keys
+
+service = Service(executable_path=binary_path)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=service, options=options)
+base_url = ("https://dineshvelhal.github.io/testautomation-playground")
+driver.get(f"{base_url}/forms.html")
+driver.find_element('id','check_javascript').click()
+checke1 = driver.find_element('id','check_validate').text
+assert checke1 == "JAVASCRIPT"
+driver.find_element('id','notes').send_keys("helo test")
+checke2 = driver.find_element('id','area_notes_validate').text
+assert checke2 == "helo test"
